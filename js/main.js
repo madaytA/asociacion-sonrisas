2b7ba76deb3170e36d6b805e32709a63566e615c
SIS-LAP-000\Sis. Toshiba <katyfigueroam.2@gmail.com>
2017-04-21 17:57:50 -0500

    page-creditos

--- js/main.js
+++ js/main.js
@@ -77,6 +77,12 @@ $(function() {
   });
 });
 
+// -------accordion credit----------------
+$("#leftside-navigation .sub-menu > a").click(function(e) {
+  $("#leftside-navigation ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
+  // e.stopPropagation()
+  e.preventDefault();
+})
 // <.--------scrollup-------------
 $(document).ready(function(){ 
         $(window).scroll(function(){
